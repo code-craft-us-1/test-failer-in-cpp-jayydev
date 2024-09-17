86be141c4a691135659d4d1bd97b782c69a62504
Jay <jay.singh@philips.com>
2024-09-17 23:19:12 +0530

    fix compilation issue

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -2,8 +2,39 @@
 #include <assert.h>
 #include <string>
 #include <iostream>
-#include "./sensorStub.h"
 
+using std::string;
+using WeatherSpace::SensorStub;
+
+SensorStub::SensorStub()
+    : m_Humidity(72),
+    m_Precipitation(70),
+    m_Temperature(26),
+    m_WindSpeed(52) {
+}
+
+SensorStub::SensorStub(int humidity, int precipitation, int temperature, int windSpeed)
+    : m_Humidity(humidity),
+    m_Precipitation(precipitation),
+    m_Temperature(temperature),
+     m_WindSpeed(windSpeed) {
+}
+
+int SensorStub::Humidity() const {
+    return m_Humidity;
+}
+
+int SensorStub::Precipitation() const {
+    return m_Precipitation;
+}
+
+double SensorStub::TemperatureInC() const {
+    return m_Temperature;
+}
+
+int SensorStub::WindSpeedKMPH() const {
+    return m_WindSpeed;
+}
 
 // This is a function to predict the weather, based on readings from a sensor
 std::string WeatherSpace::Report(const IWeatherSensor &sensor) {
--- weatherreport.h
+++ weatherreport.h
@@ -2,15 +2,29 @@
 
 #include <string>
 
-namespace WeatherSpace
-{
-    class IWeatherSensor
-    {
-    public:
+namespace WeatherSpace {
+    class IWeatherSensor {
+        public:
         virtual double TemperatureInC() const = 0;
         virtual int Precipitation() const = 0;
         virtual int Humidity() const = 0;
         virtual int WindSpeedKMPH() const = 0;
+    };
+
+    class SensorStub : public IWeatherSensor {
+    public:
+        SensorStub();
+        ~SensorStub() = default;
+        SensorStub(int humidity, int precipitation, int temperature, int windSpeed);
+        int Humidity() const override;
+        int Precipitation() const override;
+        double TemperatureInC() const override;
+        int WindSpeedKMPH() const override;
+    private:
+        int m_Humidity = 72;
+        int m_Precipitation = 70;
+        double m_Temperature = 26;
+        int m_WindSpeed = 52;
     };
 
     // This is a function to predict the weather, based on readings from a sensor
