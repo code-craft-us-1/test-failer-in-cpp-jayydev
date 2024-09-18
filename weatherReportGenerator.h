4a7e84398cff86862f3231b7ff42ac4281358704
Jay <jay.singh@philips.com>
2024-09-18 12:17:42 +0530

    fix error

--- weatherReportGenerator.h
+++ weatherReportGenerator.h
@@ -3,7 +3,7 @@
 
 namespace WeatherSpace {
     // This is a function to predict the weather, based on readings from a sensor
-    std::string WeatherSpace::Report(const IWeatherSensor &sensor) {
+    std::string Report(const IWeatherSensor &sensor) {
         int precipitation = sensor.Precipitation();
         // precipitation < 20 is a sunny day
         std::string report = "Sunny day";
